eb6d6db52155eb4ddd6e0a5dfb9e9b97cebbd208
molebox <hello@richardhaines.dev>
2022-06-13 11:41:03 +0200

    edge function

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,39 @@
+// eslint-disable-next-line @next/next/no-server-import-in-page
 import { NextResponse } from 'next/server'
+// eslint-disable-next-line @next/next/no-server-import-in-page
 import type { NextRequest } from 'next/server'
+// eslint-disable-next-line @next/next/no-server-import-in-page
+import type { NextFetchEvent } from 'next/server'
 
 
 export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname === '/') {
-    return NextResponse.redirect(new URL('/about', request.url))
-  }
+  //return NextResponse.rewrite(new URL('/about-2', request.url));
+
+  const reqCookies = request.cookies
+
+  reqCookies.get('lang')
+
+  const response = NextResponse.next()
+  // set a cookie
+  response.cookies.set('vercel', 'fast')
+
+  // set another cookie with options
+  response.cookies.set('nextjs', 'awesome', { path: '/about' })
+
+  // get all the details of a cookie
+  const { value, options } = response.cookies.getWithOptions('vercel')
+  console.log(value) // => 'fast'
+  console.log(options) // => { Path: '/test' }
+
+  // deleting a cookie will mark it as expired
+  response.cookies.delete('vercel')
+
+  // clear all cookies means mark all of them as expired
+  response.cookies.clear()
+  return response
+}
+
+// config with custom matcher
+export const config = {
+  matcher: '/about/:path*'
 }
---
+++ pages/about-2.tsx
@@ -0,0 +1,8 @@
+export default function About2() {
+  return (
+    <div>
+      <h1>About 2</h1>
+      <p>This is the about page 2</p>
+    </div>
+  );
+}
